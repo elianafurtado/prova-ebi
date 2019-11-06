c1a2a19dadd1e62ff7e61c44865d5648c394ad26
leonardofurtado <srleonardofurtado@gmail.com>
2019-11-06 18:14:07 -0300

    mudando requisicao

--- ebicli/collector.py
+++ ebicli/collector.py
@@ -1,40 +1,44 @@
 import requests
+import sys
 
-class Collector:
-    URL = "https://www.receitaws.com.br/v1/cnpj/"
-    def __init__(self, cnpj):
-        self.cnpj = cnpj
-        self.request = requests.get(self.URL + self.cnpj).json()
 
-    def get_atividade_principal(self):
-        return self.request["atividade_principal"][0]['text']
+def request(cnpj):
+    endpoint = "https://www.receitaws.com.br/v1/cnpj/"
+    url = endpoint + cnpj
+    try:
+        request = requests.get(url).json()
+        return request
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
 
-    def get_atividades_secundarias(self):
-        for item in self.request["atividades_secundarias"]:
-            print(item['text'])
-    def get_nome(self):
-        return self.request["nome"]
 
-    def get_uf(self):
-        return self.request["uf"]
+def imprimir_dados(dados):
+    if "message" not in dados:
+        print("Nome: ", dados["nome"])
+        print("UF: ", dados["uf"])
+        print("Telefone: ", dados["telefone"])
+        print("Email: ", dados["email"])
+        print("Data de abetura: ", dados["abertura"])
+        print("\n\n")
+        print(
+            "Atividade principal:\n", dados["atividade_principal"][0]["text"]
+        )
+        print(15 * "-")
+        print("\n\n")
+        print("Atividades secundarias:")
+        for item in dados["atividades_secundarias"]:
+            print(item["text"])
+            print(15 * "-", "\n")
+    else:
+        print(dados["message"])
 
-    def get_telefone(self):
-        return self.request["telefone"]
 
-    def get_email(self):
-        return self.request["email"]
+def main():
+    cnpj = input("Insira um CNPJ:")
+    dados = request(cnpj)
+    imprimir_dados(dados)
 
-    def get_data_de_abertura(self):
-        return self.request["abertura"]
 
-if __name__ == '__main__':
-    cnpj = input('informe um cnpj:')
-    c = Collector(cnpj)
-    print("Nome: ", c.get_nome())
-    print("\nUF: ", c.get_uf())
-    print("\nTelefone: ", c.get_telefone())
-    print("\nEmail: ", c.get_email())
-    print("\nData de abertura: ", c.get_data_de_abertura())
-    print("\nAtividade principal: ", c.get_atividade_principal())
-    #print("\nAtividades Secundarias: ", c.get_atividades_secundarias())
-
+if __name__ == "__main__":
+    main()
